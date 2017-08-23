4cb70b03ad701baf8cfa4f32486a407f427b7fc9
Brett Terpstra <me@brettterpstra.com>
2017-08-23 13:11:19 -0500

    minor tweaks, add control bar on hover

--- package.sh
+++ package.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Prepare jacket for Simplify 2.5
 ver=$(cat com.brettterpstra.sidecar.jacket/index.html | grep "jacket.version" | sed -E 's/^.*"([0-9\.]+)".*/\1/')
-zip -r sidecar.zip com.brettterpstra.sidecar.jacket -x .DS_Store
-mv sidecar.zip ~/Desktop/Sidecar.jacket
-cd ~/Desktop
-zip -r sidecar${ver}.zip Sidecar.jacket
-rm ~/Desktop/Sidecar.jacket
+cp -r "com.brettterpstra.sidecar.jacket" ~/Desktop/Sidecar${ver}
+zip -r ~/Desktop/Sidecar${ver}.zip ~/Desktop/Sidecar${ver} -x .DS_Store
+# mv sidecar.zip ~/Desktop/Sidecar.jacket
+# cd ~/Desktop
+# zip -r sidecar${ver}.zip Sidecar.jacket
+# rm ~/Desktop/Sidecar.jacket
